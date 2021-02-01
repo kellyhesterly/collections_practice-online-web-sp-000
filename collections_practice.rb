2075765238e91a06c0aae8de25abc0778c1ac7d6
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-01 21:52:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,12 +1,13 @@
 def sort_array_asc(array)
-  array.sort {|x , y|
+  array.sort {|x, y|
       x <=> y }
 end
 
 def sort_array_desc(array)
-  array.sort {|x , y|
+  array.sort {|x, y|
       y <=> x }
 end
 
 def sort_array_char_count(array)
+  array.sort do |x, y|
 end
