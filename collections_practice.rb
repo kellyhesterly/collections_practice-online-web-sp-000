ac513c49be0ca0a208f417ddb43677574604e3c3
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-01 22:36:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,11 +27,10 @@ def kesha_maker(array)
 end
 
 def find_a(array)
-  new_array = []
+  # new_array = []
   array.collect do |word|
   if word.start_with?('a')
     return word
   end
-  new_array
   end
 end
